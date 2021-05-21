fc78672d64e92a63f6c0e8129670c0712e63bc6e
John-D-S <johnduncanstevenson@gmail.com>
2021-05-21 15:52:28 +1000

    added links file with gdd link

--- Assets/Tower Defence/Scripts/CameraMovement.cs
+++ Assets/Tower Defence/Scripts/CameraMovement.cs
@@ -2,54 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
+namespace Controls
 {
-    [SerializeField]
-    private GameObject camera;
-    
-    [Header("Position Settings")]
-    [SerializeField]
-    private float pivotHeight = 5;
-    [SerializeField]
-    private float MinDistance = 10;
-    [SerializeField]
-    private float MaxDistance = 500;
-    [SerializeField]
-    private float DefaultDistance = 100f;
-
-    [Header("Rotation Settings")]
-    //the maximum angle from the ground
-    [SerializeField]
-    private float maxAngle = 90;
-    [SerializeField]
-    private float minAngle = 10;
-    [SerializeField]
-    private float defaultAngle = 45;
-
-
-    private void OnValidate()
+    public class CameraMovement : MonoBehaviour
     {
-        gameObject.transform.rotation = Quaternion.Euler(Vector3.right * defaultAngle); 
-        
-        //if the camera is assigned, correctly set its position;
-        if (camera)
+        [SerializeField]
+        private GameObject camera;
+    
+        [Header("Position Settings")]
+        [SerializeField]
+        private float pivotHeight = 5;
+        [SerializeField]
+        private float MinDistance = 10;
+        [SerializeField]
+        private float MaxDistance = 500;
+        [SerializeField]
+        private float DefaultDistance = 100f;
+
+        [Header("Rotation Settings")]
+        //the maximum angle from the ground
+        [SerializeField]
+        private float maxAngle = 90;
+        [SerializeField]
+        private float minAngle = 10;
+        [SerializeField]
+        private float defaultAngle = 45;
+
+
+        private void OnValidate()
         {
-            if (camera.transform.parent == gameObject.transform)
-                camera.transform.parent = gameObject.transform;
-            camera.transform.localPosition = Vector3.back * DefaultDistance;
+            gameObject.transform.rotation = Quaternion.Euler(Vector3.right * defaultAngle);
+
+            //if the camera is assigned, correctly set its position;
+            if (camera)
+            {
+                if (camera.transform.parent == gameObject.transform)
+                    camera.transform.parent = gameObject.transform;
+                camera.transform.localPosition = Vector3.back * DefaultDistance;
+            }
         }
 
-    }
+        float TargetHeight(Vector2 _targetPosition)
+        {
+            LayerMask terrain = LayerMask.GetMask("Terrain");
+            RaycastHit hit;
+            if (Physics.Raycast(gameObject.transform.position + Vector3.up * 10f, Vector3.down, out hit, Mathf.Infinity, terrain))
+            {
+                return hit.point.y;
+            }
+            else return 0;
+        }
 
-    // Start is called before the first frame update
-    void Start()
-    {
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
         
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
     }
 }
